da056f7b374029b03c399eca64d1736823c3a6b3
Eugene Lazin <4lazin@gmail.com>
2017-11-10 16:53:34 +0300

    Fix top-N query UI

--- datasource.ts
+++ datasource.ts
@@ -52,7 +52,7 @@ class AkumuliDatasource {
 
   /** Parse series name in a canonical form */
   extractTags(names) {
-    var where = {};
+    var where = [];
     _.forEach(names, name => {
       var tags = name.split(' ');
       if (tags.length < 2) {
@@ -60,16 +60,14 @@ class AkumuliDatasource {
         // contain a metric name and at least one tag.
         throw "bad metric name received";
       }
-      var metric = tags[0];
+      var tagset = {};
       for (var i = 1; i < tags.length; i++) {
         var kv = tags[i].split('=');
         var tag = kv[0];
         var value = kv[1];
-        if (!where[tag]) {
-          where[tag] = [];
-        }
-        where[tag].push(value);
+        tagset[tag] = value;
       }
+      where.push(tagset);
     });
     return where;
   }
@@ -156,6 +154,74 @@ class AkumuliDatasource {
     });
   }
 
+  /** Query time-series storage */
+  groupAggregateTopNQuery(begin, end, interval, limit, target) {
+    // Use all the same parametres as original query
+    // but add 'top' function to the 'apply' clause.
+    // Extract tags from results and run 'select' query
+    // nomrally.
+    var metricName = target.metric;
+    var tags = target.tags;
+    var isTop = target.topN ? true : false;
+    var topN = target.topN;
+    if (!isTop) {
+      throw "top-N parameter required";
+    }
+    var query: any = {
+      select: metricName,
+      range: {
+        from: begin.format('YYYYMMDDTHHmmss.SSS'),
+        to: end.format('YYYYMMDDTHHmmss.SSS')
+      },
+      where: tags,
+      "order-by": "series",
+      apply: [{name: "top", N: topN}]
+    };
+
+    var httpRequest: any = {
+      method: "POST",
+      url: this.instanceSettings.url + "/api/query",
+      data: query
+    };
+
+    return this.backendSrv.datasourceRequest(httpRequest).then(res => {
+      var data = [];
+      if (res.status === 'error') {
+        throw res.error;
+      }
+      if (res.data.charAt(0) === '-') {
+        throw { message: "Query error: " + res.data.substr(1) };
+      }
+      var lines = res.data.split("\r\n");
+      var index = 0;
+      var series = null;
+      var timestamp = null;
+      var value = 0.0;
+      var datapoints = [];
+      var currentTarget = null;
+      var series_names = [];
+      _.forEach(lines, line => {
+        let step = index % 3;
+        if (step === 0) {
+            // parse series name
+            series = line.substr(1);
+            if (series) {
+              series_names.push(series);
+            }
+        }
+        index++;
+      });
+      var newTarget = {
+        metric: metricName,
+        tags: this.extractTags(series_names),
+        shouldComputeRate: target.shouldComputeRate,
+        shouldEWMA: target.shouldEWMA,
+        decay: target.decay,
+        downsampleAggregator: target.downsampleAggregator
+      };
+      return this.groupAggregateTargetQuery(begin, end, interval, limit, newTarget);
+    });
+  }
   /** Query time-series storage */
   groupAggregateTargetQuery(begin, end, interval, limit, target) {
     var metricName = target.metric;
@@ -258,7 +324,7 @@ class AkumuliDatasource {
     // nomrally.
     var metricName = target.metric;
     var tags = target.tags;
-    var isTop = target.shouldTop;
+    var isTop = target.topN ? true : false;
     var topN = target.topN;
     if (!isTop) {
       throw "top-N parameter required";
@@ -304,7 +370,6 @@ class AkumuliDatasource {
             if (series) {
               series_names.push(series);
             }
-            console.log("Series name: %s", series);
         }
         index++;
       });
@@ -315,7 +380,6 @@ class AkumuliDatasource {
         shouldEWMA: target.shouldEWMA,
         decay: target.decay,
       };
-      console.log(newTarget);
       return this.selectTargetQuery(begin, end, limit, newTarget);
     });
   }
@@ -414,7 +478,7 @@ class AkumuliDatasource {
     var limit    = options.maxDataPoints;  // TODO: don't ignore the limit
     var allQueryPromise = _.map(options.targets, target => {
       var disableDownsampling = target.disableDownsampling;
-      var isTop = target.shouldTop;
+      var isTop = target.topN ? true : false;
       if (disableDownsampling) {
         if (isTop) {
           return this.selectTopNQuery(begin, end, limit, target);
@@ -422,7 +486,11 @@ class AkumuliDatasource {
           return this.selectTargetQuery(begin, end, limit, target);
         }
       } else {
-        return this.groupAggregateTargetQuery(begin, end, interval, limit, target);
+        if (isTop) {
+          return this.groupAggregateTopNQuery(begin, end, interval, limit, target);
+        } else {
+          return this.groupAggregateTargetQuery(begin, end, interval, limit, target);
+        }
       }
     });
 
